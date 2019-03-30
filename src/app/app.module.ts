cf40e47f990efac6b10313328c978a82f64819f7
DgDos <fijusohalo@gmail.com>
2019-03-30 18:24:43 -0500

    Mi primer commit en Angular :3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { FabianComponent } from './fabian/fabian.component';
+import { JaimeComponent } from './jaime/jaime.component';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+  { path: 'heroe', component: JaimeComponent},
+  {path: 'villano', component: FabianComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FabianComponent,
+    JaimeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
+
 export class AppModule { }
---
+++ src/app/jaime/jaime.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jaime',
+  templateUrl: './jaime.component.html',
+  styleUrls: ['./jaime.component.css']
+})
+export class JaimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
